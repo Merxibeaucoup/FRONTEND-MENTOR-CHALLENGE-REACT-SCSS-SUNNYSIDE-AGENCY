3b8b1eb63bf3f6ad435ecf24bf32a85fdfa30962
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2022-10-02 00:23:35 -0400

    third

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import Nav from "./Nav";
+
 const Header = () => {
+  const bgImage = "./assets/images/desktop/image-header.jpg";
+
+  const dArrow = "./assets/images/icon-arrow-down.svg";
   return (
-    <div>
+    <div
+      className="header"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "cover",
+      }}
+    >
       <Nav />
+
+      <div className="header__contents">
+        <h2 className="header__contents__heading">we are creatives</h2>
+        <div className="header__contents__arrow">
+          <img src={dArrow} alt="arrow down" />
+        </div>
+      </div>
     </div>
   );
 };
