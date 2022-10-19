349c3a3e39ecd70b3e5e42a5b88bd9f7a5a23b47
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2022-10-19 00:59:16 -0400

    nav added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,24 +14,26 @@ const Nav = () => {
         <div className="nav__logo">
           <img src="assets/images/logo.svg" alt="" />
         </div>
-        <ul ref={navRef} className="nav__items">
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/services">Services</a>
-          </li>
-          <li>
-            <a href="/projects">Projects</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
+        <nav>
+          <ul ref={navRef} className="nav__items">
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/services">Services</a>
+            </li>
+            <li>
+              <a href="/projects">Projects</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
 
-          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-            <FaTimes />
-          </button>
-        </ul>
+            <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+              <FaTimes />
+            </button>
+          </ul>
+        </nav>
         <button className="nav-btn " onClick={showNavbar}>
           <FaBars />
         </button>
